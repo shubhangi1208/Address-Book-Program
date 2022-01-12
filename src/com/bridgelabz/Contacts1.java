695c3eb4de2e3c509b55b5607e17619a8fad5ded
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-12 15:20:44 +0530

    ADDED:Contacts,AddressBook Class and Variable

---
+++ src/com/bridgelabz/Contacts1.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Contacts1 {
+    String firstname;
+    String lastname;
+    String address;
+    String city;
+    String zip;
+    String state;
+    String phone_number ;
+    String email;
+}
