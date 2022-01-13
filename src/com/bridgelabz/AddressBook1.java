9c4a43c5698d3f019e7975850bb2ae6fe1c45455
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-13 10:43:17 +0530

    ADDED:New Contacts and Display contact method created

--- src/com/bridgelabz/AddressBook1.java
+++ src/com/bridgelabz/AddressBook1.java
@@ -1,4 +1,53 @@
 package com.bridgelabz;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class AddressBook1 {
+    //Created Scanner class object
+    Scanner sc = new Scanner(System.in);
+
+    //ArrayList created for storing contacts
+    ArrayList<Contacts1> list = new ArrayList<Contacts1>();
+
+    public void addContacts() {
+
+        Contacts1 contacts = new Contacts1();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the Contact details");
+        System.out.println("Enter the First Name :");
+        contacts.setFirstname(sc.next());
+        System.out.println("Enter the Last Name :");
+        contacts.setLastname(sc.next());
+        System.out.println("Enter the Address :");
+        contacts.setAddress(sc.next());
+        System.out.println("Enter the City :");
+        contacts.setCity(sc.next());
+        System.out.println("Enter the State :");
+        contacts.setState(sc.next());
+        System.out.println("Enter the Zip Code :");
+        contacts.setZip(sc.next());
+        System.out.println("Enter the Phone Number :");
+        contacts.setPhone_number(sc.next());
+        System.out.println("Enter the EMail ID :");
+        contacts.setEmail(sc.next());
+        list.add(contacts);
+    }
+
+    //Method to Show the Contact Details
+    public void showContacts() {
+        for (Contacts1 contacts : list) {
+            System.out.println("Contact Details -");
+            System.out.println("First Name : " + contacts.getFirstname());
+            System.out.println("Last Name : " + contacts.getLastname());
+            System.out.println("Address : " + contacts.getAddress());
+            System.out.println("City : " + contacts.getCity());
+            System.out.println("State : " + contacts.getState());
+            System.out.println("Zip Code : " + contacts.getZip());
+            System.out.println("Phone Number : " + contacts.getPhone_number());
+            System.out.println("EMail ID : " + contacts.getEmail());
+        }
+
+    }
+
+
 }
--- src/com/bridgelabz/AddressBookMain1.java
+++ src/com/bridgelabz/AddressBookMain1.java
@@ -1,9 +1,32 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain1 {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
-
+        int choice;
+        AddressBook1 Ab = new AddressBook1();
+        do {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Welcome to Address Book Program");
+            System.out.println("Kindly Enter choice \n 1. Add new Contact \n 2. Edit Contact details \n 3. Display details  \n 4 Delete Contact \n 5. Quit");
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    Ab.addContacts();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    Ab.showContacts();
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+        }while (choice != 5);
     }
+
 }
